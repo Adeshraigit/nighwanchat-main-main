f150b800fc511946866397eab91269e65d10bc88
karim1556 <karimshaikh1555@gmail.com>
2025-03-25 21:21:38 +0530

    helmet

--- NighwanReact/src/Pages/Homepage.jsx
+++ NighwanReact/src/Pages/Homepage.jsx
@@ -10,10 +10,16 @@ import NewsArea from '../components/Home/NewsArea';
 import FeatureArea from '../components/Home/FeatureArea'; 
 import ContactArea from '../components/Home/ContactArea';   
 import Service4Area from '../components/About/Service4Area';
+import { Helmet } from "react-helmet";
 
 const Homepage = () => {
   return (
     <>
+      <Helmet>
+  <title>Nighwan Technology Pvt. Ltd. | Digital Transformation & Software Development</title>
+  <meta name="description" content="Nighwan Technology Pvt. Ltd. is a leading software development and digital transformation company providing AI-driven IT solutions, ERP systems, web development, and lean manufacturing consultancy." />
+  <meta name="keywords" content="digital transformation, software development, IT consulting, AI solutions, web development, enterprise software, ERP systems, SaaS solutions, lean manufacturing technology" />
+</Helmet>
       <HeroEmpowerment />
       <ClientArea/>
       <HowWeDo/>
